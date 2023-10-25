efde99dba09b687c65ed8ff2d2114c73f0e7f6be
SNKK62 <koki.seno62@gmail.com>
2023-10-26 01:54:06 +0900

    extract routing to nux

--- main.go
+++ main.go
@@ -5,19 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
 	"github.com/SNKK62/go_todo_app/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func run(ctx context.Context) error {
-	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM)
-	defer stop()
 	cfg, err := config.New()
 	if err != nil {
 		return err
@@ -28,36 +21,9 @@ func run(ctx context.Context) error {
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
-	s := &http.Server{
-		// don't use Addr field because of using net.Listener arg
-		// Addr: ":18080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// for experiment
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-
-	//launch HTTP server in another Goroutine
-	eg.Go(func() error {
-		// replace ListernAndServe with Serve
-		if err := s.Serve(l); err != nil &&
-			//http.ErrServerClosed is not an error, but is signal for terminating http.Server.Shutdown() normally
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	// waiting for end signal from channel
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	//waiting for end of another Goroutine with Go method.
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
 
 func main() {
