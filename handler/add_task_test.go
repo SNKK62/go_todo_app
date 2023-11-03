4d55b4b5f4fc81a26998d2ed365c42ae1dd54324
SNKK62 <koki.seno62@gmail.com>
2023-11-04 00:41:52 +0900

    complete chapter19

--- handler/add_task_test.go
+++ handler/add_task_test.go
@@ -2,12 +2,13 @@ package handler
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/SNKK62/go_todo_app/entity"
-	"github.com/SNKK62/go_todo_app/store"
 	"github.com/SNKK62/go_todo_app/testutil"
 	"github.com/go-playground/validator/v10"
 )
@@ -48,11 +49,19 @@ func TestAddTask(t *testing.T) {
 				bytes.NewReader(testutil.LoadFile(t, tt.reqFile)),
 			)
 
+			moq := &AddTaskServiceMock{}
+			moq.AddTaskFunc = func(
+				ctx context.Context, title string,
+			) (*entity.Task, error) {
+				if tt.want.status == http.StatusOK {
+					return &entity.Task{ID: 1}, nil
+				}
+				return nil, errors.New("error from mock")
+			}
 			sut := AddTask{
-				Store: &store.TaskStore{
-					Tasks: map[entity.TaskID]*entity.Task{},
-				},
-				Validator: validator.New()}
+				Service:   moq,
+				Validator: validator.New(),
+			}
 			sut.ServeHTTP(w, r)
 
 			resp := w.Result()
